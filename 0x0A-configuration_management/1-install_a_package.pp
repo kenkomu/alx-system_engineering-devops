44bfd2ed7242f45f25f405d49864af9f082bf616
kenkomu <komukenneth216@gmail.com>
2022-10-30 15:02:57 +0300

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,5 @@
 # install flask from pip3.
 exec { 'install python packages':
     ensure  => '0.8.1',
-    path    => ['/usr/bin/'],
-    unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
-
     command => 'pip3 install flask flask_restful apiai',
     }
