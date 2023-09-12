c50e5a4bc12609d85aca5e34fc6b5d414a1db681
kenkomu <komukenneth216@gmail.com>
2023-09-12 13:15:09 +0300

    initial commit

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,95 +1,118 @@
 #!/usr/bin/python3
-""" querry reddit api for subreddit info
 """
+    Query the Reddit API,
+    parses the title of all hot articles,
+    and prints a sorted count of given keywords
+"""
+
 import requests
-import requests.auth
-import string
-from time import sleep
 
 
-def authenticate():
-    """ authenticate function
-    doesnt take parameters returns token_type and access_token
-    """
-    usr_name = "jgadelugo"
-    temp = "HolbertonPass845"
-
-    secret = "Z4Sa9bA6RRE44qDyhHQiTlW1gd0"
-    client_id = "hy4KvoK0W2iDvw"
-
-    client_auth = requests.auth.HTTPBasicAuth(client_id, secret)
-    post_data = {"grant_type": "password",
-                 "username": usr_name,
-                 "password": temp}
-
-    headers = {"User-Agent": "ChangeMeClient/0.1 by {}".format(usr_name)}
-    response = requests.post("https://www.reddit.com/api/v1/access_token",
-                             auth=client_auth, data=post_data, headers=headers)
-    auth_json = response.json()
-
-    token_type = auth_json['token_type']
-    access_token = auth_json['access_token']
-
-    return (token_type, access_token)
-
-
-def recurse(subreddit, hot_list=[], after=[], t_type=None, a_token=None):
-    """ querry reddit api for hot post
-    recursively get all hot post from subreddit
-    """
-    sub = subreddit
-    subreddit = "/r/{}/hot".format(sub)
-    usr_name = "jgadelugo"
-
-    if len(after) == 0:
-        t_type, a_token = authenticate()
-
-    headers = {"Authorization": "{} {}".format(t_type, a_token),
-               "User-Agent": "ChangeMeClient/0.1 by {}".format(usr_name)}
-    if len(after) != 0:
-        param = {"limit": 100, "after": after[-1]}
-    else:
-        param = {"limit": 100}
-
-    sleep(1)
-    query = "https://oauth.reddit.com{}".format(subreddit)
-    res = requests.get(query, headers=headers, params=param)
-
-    status = res.status_code
-
-    if (status != 200):
+def convert_lower(w, i, len_w):
+    """Convert strings to lowercase"""
+    if i >= len_w:
         return None
-    else:
-        data = res.json()
-        if data['data']['after'] in after:
-            return hot_list
-        after.append(data['data']['after'])
-        posts = data["data"]['children']
-        for post in posts:
-            hot_list.append(post['data']['title'])
 
-        return recurse(sub, hot_list, after, t_type, a_token)
+    w[i] = w[i].lower()
+    convert_lower(w, i + 1, len_w)
+
+    return None
+
+
+def count_word_title(title, i, len_t, dict_words_unique):
+    """Store repeating string in hash table"""
+    if i >= len_t:
+        return None
+
+    if title[i] in dict_words_unique:
+        dict_words_unique[title[i]] += 1
+
+    count_word_title(title, i + 1, len_t, dict_words_unique)
+
+    return None
+
+
+def store_title_word_hash(list_title, i, len_l, dict_words_unique):
+    """Run trough a list of hot titles"""
+    if i >= len_l:
+        return None
+
+    title = list_title[i]["data"]["title"].lower().split(" ")
+
+    count_word_title(title, 0, len(title), dict_words_unique)
+
+    store_title_word_hash(list_title, i + 1, len_l, dict_words_unique)
+
+    return None
+
+
+def store_given_word_hash(w, i, len_w, dict_words, type_d):
+    """store given words in hash tables"""
+    if i >= len_w:
+        return None
+    if type_d == "r":
+        if w[i] in dict_words:
+            dict_words[w[i]] += 1
+        else:
+            dict_words[w[i]] = 1
+    elif type_d == "u":
+        dict_words[w[i]] = 0
+
+    store_given_word_hash(w, i + 1, len_w, dict_words, type_d)
+
+    return None
+
+
+def print_words(w, i, len_wn, dict_words_repeated):
+    """Print the repeated strings"""
+    if i >= len_wn:
+        return None
+
+    if w[i][1] > 0:
+        print("{}: {}".format(w[i][0], dict_words_repeated[w[i][0]] * w[i][1]))
+
+    print_words(w, i + 1, len_wn, dict_words_repeated)
+
+    return None
+
+
+def get_all_hot_articles(subreddit, word_list, dict_words_unique, after=""):
+    """Get the all articles"""
+    if after is None:
+        return dict_words_unique
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url += "?limit=100&after={}".format(after)
+    headers_m = {'User-agent': 'botsito'}
+    redirect = False
+
+    r = requests.get(url, allow_redirects=redirect, headers=headers_m)
+    if r.status_code != 200:
+        return None
+
+    articles = r.json().get("data").get("children")
+
+    store_title_word_hash(articles, 0, len(articles), dict_words_unique)
+
+    after = r.json().get("data").get("after")
+    return get_all_hot_articles(subreddit, word_list, dict_words_unique, after)
 
 
 def count_words(subreddit, word_list):
-    """ count words """
-    flag = 0
-    words = {}
-    for word in word_list:
-        words[word] = 0
-    hot_list = recurse(subreddit)
-    if hot_list is None:
+    """Count the number of time a word in word_list is name
+    in the hot articles subrredt"""
+    convert_lower(word_list, 0, len(word_list))
+
+    words_repeated = {}
+    store_given_word_hash(word_list, 0, len(word_list), words_repeated, "r")
+
+    words_unique = {}
+    store_given_word_hash(word_list, 0, len(word_list), words_unique, "u")
+
+    words_filled = get_all_hot_articles(subreddit, word_list, words_unique)
+    if not words_filled:
         return
-    for hot in hot_list:
-        hot.translate(str.maketrans('', '', string.punctuation))
-        for h in hot.lower().split():
-            for word in word_list:
-                if h.lower() == word.lower():
-                    words[word] += 1
-    sorted_words = sorted(words.items(), key=lambda x: (-x[1], x[0]))
-    for key, value in sorted_words:
-        if value != 0:
-            print("{}: {}".format(key, value))
-            flag = 1
-    if flag == 0:
-        print()
+
+    sort_words = sorted(words_filled.items(), key=lambda t: t[::-1])
+    sort_des_words = sorted(sort_words, key=lambda tup: tup[1], reverse=True)
+
+    print_words(sort_des_words, 0, len(sort_des_words), words_repeated)
