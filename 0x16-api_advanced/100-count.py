0fae063cbe8fb9ce500499007a0b3568d0c63cc8
kenkomu <komukenneth216@gmail.com>
2023-09-12 13:18:18 +0300

    initial commit

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
-# Searches for keywords in hot titles
-import requests
+"""A file to make a query to an endpoint"""
+from requests import request
 
 
-def count_words(subreddit, word_list, count=0, after_val=None):
-    '''Searches for keywords in hot titles'''
-    URL = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    USERAGENT = {'User-Agent':
-                 'Unix:com.holberton.apiadvanced:task3 (by /u/_marc_marc_)'}
-    params = {'limit': 100}
+def count_words(subreddit, word_list, after="", counter={}, ini=0):
+    """A recursive function that queries the Reddit API, parses the title
+    of all hot articles, and prints a sorted count of given keywords
+    (case-insensitive, delimited by spaces. Javascript should count as
+    javascript, but java should not)
+    """
+    if ini == 0:
+        for word in word_list:
+            counter[word] = 0
+
+    url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
+    headers = {"User-Agent": "Python3"}
+    response = request("GET", url, headers=headers).json()
+    try:
+        top = response['data']['children']
+        _after = response['data']['after']
+        for item in top:
+            for word in counter:
+                counter[word] += item['data']['title'].lower(
+                    ).split(' ').count(word.lower())
+        if _after is not None:
+            count_words(subreddit, word_list, _after, counter, 1)
+        else:
+            str = sorted(counter.items(), key=lambda kv: kv[1], reverse=True)
+            for name, num in str:
+                if num != 0:
+                    print('{}: {}'.format(name, num))
+    except Exception:
+        return None
